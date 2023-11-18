4a48cc7c1ecb2cae2a4661c055962d08111ffbd4
Justice Arthur <justicea83@gmail.com>
2023-11-17 20:24:51 -0500

    save deck to file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -19,8 +23,16 @@ func newDeck() deck {
 	return cards
 }
 
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
 func (d deck) print() {
