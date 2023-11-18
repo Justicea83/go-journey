cfd5298b608430f16fad86a3a9f26836de53b0e7
Justice Arthur <justicea83@gmail.com>
2023-11-17 23:26:57 -0500

    add updates

--- structs/main.go
+++ structs/main.go
@@ -13,22 +13,21 @@ type person struct {
 	contactInfo
 }
 
-func (p person) print() {
+func (p *person) print() {
 	fmt.Printf("%+v", p)
 }
 
-func (pointerToPerson *person) updateFirstName(firstName string) {
-	(*pointerToPerson).firstName = firstName
+func (p *person) updateFirstName(firstName string) {
+	p.firstName = firstName
 }
 
 func main() {
-	jim := person{
+	jimPtr := person{
 		firstName:   "Jim",
 		lastName:    "Essiet",
 		contactInfo: contactInfo{email: "jim@gmail.com", zipCode: 00234},
 	}
 
-	jimPointer := &jim
-	jimPointer.updateFirstName("Jimmy")
-	jim.print()
+	jimPtr.updateFirstName("Jimmy")
+	jimPtr.print()
 }
