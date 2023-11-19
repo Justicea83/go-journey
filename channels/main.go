2f04d4655ad10e94ba08395952f5b5418dc97b22
Justice Arthur <justicea83@gmail.com>
2023-11-18 20:32:33 -0500

    sequential codes

---
+++ channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://amazon.com",
+		"http://golang.org",
+		"http://taskflowhr.com",
+	}
+
+	for _, link := range links {
+		ping(link)
+	}
+}
+
+func ping(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
